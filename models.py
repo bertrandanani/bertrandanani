c04fe32ad274a86396496511b55d5fc688108f1a
bertrandanani <88065236+bertrandanani@users.noreply.github.com>
2022-02-14 21:28:30 +0000

    Add files via upload

---
+++ models.py
@@ -0,0 +1,97 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
+
+database = 'library'
+passw = os.getenv('db_pass')
+
+db_path = "postgresql://postgres:bertrand@localhost:5432/library"
+
+db = SQLAlchemy()
+
+
+def setup_db(app, path=db_path):
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Category(db.Model):
+
+    __tablename__ = 'categories'
+
+    id_cat = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    libelle_categorie = db.Column(db.String(50))
+    books = db.relationship('Book', backref='categories', lazy=True)
+
+    def __init__(self, libelle_categorie):
+        self.libelle_categorie = libelle_categorie
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id_cat,
+            'categorie': self.libelle_categorie
+        }
+
+
+class Book(db.Model):
+
+    __tablename__ = 'books'
+
+    id_book = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(50), nullable=False)
+    titre = db.Column(db.String(200), nullable=False)
+    date_publication = db.Column(db.DateTime(), nullable=False)
+    auteur = db.Column(db.String(200), nullable=False)
+    editeur = db.Column(db.String(150), nullable=False)
+    categorie_id = db.Column(db.Integer, db.ForeignKey(
+        'categories.id_cat'), nullable=False)
+
+    def __init__(self, isbn, titre, date_publication, auteur, editeur, categorie_id):
+        self.isbn = isbn
+        self.titre = titre
+        self.date_publication = date_publication
+        self.auteur = auteur
+        self.editeur = editeur
+        self.categorie_id = categorie_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id_book,
+            'code_ISBN': self.isbn,
+            'titre': self.titre,
+            'auteur': self.auteur,
+            'editeur': self.editeur,
+            'date_publication': self.date_publication
+        }
+
+    
+
